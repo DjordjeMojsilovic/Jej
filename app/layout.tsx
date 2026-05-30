0e7105bfddd0ca05ad4c416370f636f66e99ac6f
Djordje.Mojsilovic <djordje.mojsilovic2011@gmail.com>
2026-05-30 19:15:51 +0200

    feat: initial project scaffold

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+import Header from '../components/Header';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
